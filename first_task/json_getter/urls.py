7a469e67465b00c08b28a9239891ebc5a55ebc29
de_brasi <ya.kakadu.ru@yandex.ru>
2023-02-01 16:28:05 +0300

    Successful routing test

---
+++ first_task/json_getter/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.example_core_page, name='start_page'),
+    path('getparamval', views.example_getter, name='example_getter'),
+]
--- first_task/json_getter/views.py
+++ first_task/json_getter/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def example_getter(request):
     return HttpResponse("Hello world!")
+
+
+def example_core_page(request):
+    return HttpResponse("I'll give some useful json")
