6019579ae22e1083dd2167752bfda8bcbdb175ed
de_brasi <ya.kakadu.ru@yandex.ru>
2023-02-02 13:09:10 +0300

    MVP with debug server

--- first_task/json_getter/views.py
+++ first_task/json_getter/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.http import HttpRequest
+
+from django.views.decorators import csrf
 
 
-def example_getter(request):
-    return HttpResponse("I'll give some useful json")
+@csrf.csrf_exempt
+def example_getter(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return JsonResponse(
+            [{"p": f"1:2:3:{i}", "v": "1,234"} for i in range(4, 99 + 1)],
+            safe=False)
+    else:
+        return HttpResponse(f"Bad request with HTTP {request.method} method.", status=405)
 
 
-def example_core_page(request):
+@csrf.csrf_exempt
+def example_core_page(request: HttpRequest):
     return HttpResponse("Hello world!")
