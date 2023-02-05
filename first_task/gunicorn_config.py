358d423b3f58eb1e83b13a8c4073ab19cdeb9dad
de_brasi <ya.kakadu.ru@yandex.ru>
2023-02-06 00:15:08 +0300

    in progress

--- first_task/gunicorn_config.py
+++ first_task/gunicorn_config.py
@@ -1,9 +1,15 @@
-import getpass
-import os
+# import getpass
+# import os
 
-command = os.getcwd() + '/../venv/bin/gunicorn'
-pythonpath = os.getcwd() + '/first_task'
+# command = os.getcwd() + '/../venv/bin/gunicorn'
+# pythonpath = os.getcwd() + '/first_task'
+
+command = '/home/ilya/WorkSpace/Projects/KASKAD_GROUP_simple_http_service/venv/bin/gunicorn'
+pythonpath = '/home/ilya/WorkSpace/Projects/KASKAD_GROUP_simple_http_service/first_task/first_task'
+
 bind = '127.0.0.1:8001'
 workers = 3
-user = getpass.getuser()
+
+# user = getpass.getuser()
+user = 'ilya'
 raw_env = 'DJANGO_SETTINGS_MODULE=first_task.settings'
