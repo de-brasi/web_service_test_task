2a615e1f3ecb52c8bce16cf76d42c6619a6c156b
de_brasi <ya.kakadu.ru@yandex.ru>
2023-02-06 12:58:08 +0300

    Relative path in bash script. Helpful comments

--- first_task/bin/start_gunicorn.sh
+++ first_task/bin/start_gunicorn.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-source /home/ilya/WorkSpace/Projects/KASKAD_GROUP_simple_http_service/venv/bin/activate
-gunicorn -c "/home/ilya/WorkSpace/Projects/KASKAD_GROUP_simple_http_service/first_task/gunicorn_config.py" first_task.wsgi
+
+#The script must be run inside a django application (not a project)!
+#Example:
+#      <Django project>
+#      ├── <Django app> - sh script must be run here!
+#      │         ├── bin
+#      │         │   └── start_gunicorn.sh
+#      │         ├── db.sqlite3
+#      │         ├── first_task
+#      │         ├── gunicorn_config.py
+#      │         ├── json_getter
+#      │         ├── manage.py
+#      │         └── __pycache__
+#      └── <Your virtualenv> - should named as "venv"
+#          ├── bin
+#          ├── lib
+#          └── pyvenv.cfg
+
+source $(pwd)/../venv/bin/activate
+gunicorn -c "$(pwd)/gunicorn_config.py" first_task.wsgi
